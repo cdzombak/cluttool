e7c5cf13b64f36a9490020b1b56c14a11d51c113
Chris Dzombak <chris@dzombak.com>
2023-01-20 08:44:35 -0500

    remove broken statements in setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 setup(
     name="cluttool",
     version="wip", # i.e. this version doesn't even work yet.
-    packages=find_packages(),
+    # packages=find_packages(),
     entry_points={
         'gui_scripts': [
             'cluttool = cluttool:cli',
         ]
     },
     install_requires=['click>=6.7<8.0'],
-    install_requires=load_requirements('requirements/base.in'),
+    # install_requires=load_requirements('requirements/base.in'),
     author="Troy Sankey",
     author_email="sankeytms@gmail.com",
     description="create and convert 3D/color LUTs",
